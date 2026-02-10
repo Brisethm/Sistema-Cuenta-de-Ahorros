2b7c1613463fe5d84b9be0ab18f6cb9c30c52edc
Briseth Marin <sofiamarin69@gmail.com>
2026-02-10 18:44:35 -0500

    Creacion repo inicial

---
+++ Sistema_Cuenta_Ahorros/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sistema_Cuenta_Ahorros.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
